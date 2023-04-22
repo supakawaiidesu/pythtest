1e7a1c1c2d0231efddb33283d4f1a1156bdacb17
Krunal Amin <kishanamin97@gmail.com>
2023-04-22 00:40:41 -0500

    init

---
+++ index.ts
@@ -0,0 +1,35 @@
+import {
+    getPythProgramKeyForCluster,
+    PriceStatus,
+    PythConnection,
+} from '@pythnetwork/client';
+import { Connection } from '@solana/web3.js';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const CLUSTER = 'pythnet';
+const connection = new Connection(process.env.RPC_URL as string);
+const pythPublicKey = getPythProgramKeyForCluster(CLUSTER);
+const pythConnection = new PythConnection(connection, pythPublicKey);
+
+pythConnection.onPriceChange((productAccount, priceAccount) => {
+    const symbol = productAccount.symbol;
+    if (symbol === 'Crypto.ETH/USD' || symbol === 'Crypto.BTC/USD') {
+        const price = priceAccount.price;
+        const confidence = priceAccount.confidence;
+        if (price && confidence) {
+            if (symbol === 'Crypto.ETH/USD') {
+                console.log(`ETH/USD price: \x1b[34m$${price.toFixed(4)}\x1b[0m`);
+            } else if (symbol === 'Crypto.BTC/USD') {
+                console.log(`BTC/USD price: \x1b[33m$${price.toFixed(4)}\x1b[0m`);
+            }
+        } else {
+            console.log(`Price for ${symbol} is not available`);
+        }
+    }
+});
+
+
+
+console.log('Listening for price changes...');
+pythConnection.start();
